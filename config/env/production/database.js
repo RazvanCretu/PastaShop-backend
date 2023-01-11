c3ed844810e159af16b1b6f3168fb36a52d2ecc7
Razvy Cretu <razvan.cretu97@gmail.com>
2023-01-11 12:19:41 +0200

    fix for connection string

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,8 +1,21 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection(
+  `${process.env.STRAPI_CLEARDB_STRING}`
+);
+
 module.exports = ({ env }) => ({
   connection: {
     client: "mysql",
     connection: {
-      connectionString: env("STRAPI_MYSQL_STRING"),
+      host: connection.config.host,
+      port: connection.config.port,
+      database: connection.config.database,
+      user: connection.config.user,
+      password: connection.config.password,
+      ssl: {
+        rejectUnauthorized: env.bool("CLEARDB_SSL", false), // For self-signed certificates
+      },
     },
   },
 });
